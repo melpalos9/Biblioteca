dbf7c2e3e32f64d4a65576b1d5010543248bf588
melpalos9 <melpalos9@gmail.com>
2025-06-02 11:53:00 -0300

    Add files via upload

--- app/apps.py
+++ app/apps.py
@@ -4,14 +4,3 @@ from django.apps import AppConfig
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views import View
-from django.contrib import messages
-
-
-class IndexView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'index.html')
-
-    def post(self, request):
-        pass
